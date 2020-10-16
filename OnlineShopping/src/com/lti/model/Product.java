0451310fe19423f2edbd99bedb13e17ef2663b7a
sritama26 <48647374+sritama26@users.noreply.github.com>
2020-10-16 10:17:00 +0530

    Update Product.java
    Changed Product class

--- OnlineShopping/src/com/lti/model/Product.java
+++ OnlineShopping/src/com/lti/model/Product.java
@@ -5,9 +5,18 @@ public class Product {
 	
 	int productId;
 	String productDesc;
+	int quantity;
+	
+	public int getQuantity(){
+		return quantity;
+	}
+	
 	public int getProductId() {
 		return productId;
 	}
+	public  void setQuantity(int quantity){
+		this.quantity=quantity;
+		
 	public void setProductId(int productId) {
 		this.productId = productId;
 	}
